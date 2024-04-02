ed2cb2ec2ea7719c02c58f2f4733a8063a3f8be3
CCranney <11773171+CCranney@users.noreply.github.com>
2024-04-02 13:52:10 -0700

    SHAP script updated with randomization seed code

--- scripts/SHAP/SHAP_run.py
+++ scripts/SHAP/SHAP_run.py
@@ -12,13 +12,16 @@ import numpy as np
 
 print ('argument list', sys.argv)
 rowIdx = int(sys.argv[1])
+randomSeed = int(sys.argv[2])
+randomSeed_dataSplit = 2
+
 #save_dir = '/common/meyerjlab/caleb_SHAP_numpy_arrays'
 save_dir = '/Users/cranneyc/Documents/Projects/CPTAC_analysis/makingABetterModel_NAS/RnaToProteinDataModule/scripts/SHAP/outputs'
 curDir = os.getcwd()
 log_dir = tempfile.mkdtemp(prefix='logs_zDELETE', dir=curDir)
-torch.manual_seed(1)
+torch.manual_seed(randomSeed)
 
-dataProcessor = StandardDatasetProcessor(random_state=1, isOnlyCodingTranscripts=False)
+dataProcessor = StandardDatasetProcessor(random_state=randomSeed_dataSplit, isOnlyCodingTranscripts=False)
 dataProcessor.debug = True
 dataProcessor.prepare_data()
 dataProcessor.synchronize_all_datasets()
@@ -31,8 +34,9 @@ background, _ = next(iter(train_loader))
 transcriptome, _ = dataProcessor.extract_full_dataset()
 
 test_images = torch.from_numpy(transcriptome.to_numpy()).to(torch.float32)
-outputFile = f'{rowIdx}_{transcriptome.index[rowIdx]}_array.npz'
+outputFile = f'{randomSeed}_{str(rowIdx).zfill(len(str(len(transcriptome))))}_{transcriptome.index[rowIdx]}_array.npz'
 print(outputFile)
+
 e = shap.DeepExplainer(model, background)
 
 shap_values = e.shap_values(test_images[rowIdx:rowIdx+1], check_additivity=False)
@@ -40,6 +44,5 @@ shap_values = np.array(shap_values)
 
 # Save the array to file
 np.savez_compressed(os.path.join(save_dir, outputFile), arr=shap_values)
-print('done')
 #loaded_array = np.load(file_path)['arr']
 
